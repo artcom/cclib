ea0898cd503588b6588a3970c47093372111b751
Sebastian Heymann <sebastian.heymann@googlemail.com>
2013-01-25 10:49:04 +0100

    fixed location of some files

---
+++ src/cclib/cclib.h
@@ -0,0 +1,32 @@
+#ifndef __CCLIB_HEADER_DEFINED__
+#define __CCLIB_HEADER_DEFINED__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tr1/memory>
+#include <GL/glew.h>
+
+#include <math/vec2.h>
+#include <math/vec3.h>
+#include <Exception.h>
+        
+// #include <gl/graphics.h>
+#include <gl/texture.h>
+#include <gl/texture2d.h>
+#include <gl/shader.h>
+#include <gl/shadertexture.h>
+#include <gl/pixelstoragemodes.h>
+#include <gl/framebufferobject.h>
+
+#include <particles/gpuconstraint.h>
+#include <particles/gpuforce.h>
+#include <particles/gpuimpulse.h>
+#include <particles/gpuparticle.h>
+#include <particles/gpuparticles.h>
+// #include <particles/gpuupdateshader.h>
+// #include <particles/gpudisplayshader.h>
+// #include <particles/gpuparticlepointrenderer.h>
+// #include <particles/gpuindexparticleemitter.h>
+
+#endif
--- src/cclib/particles/gpuparticle.h
+++ src/cclib/particles/gpuparticle.h
@@ -1,17 +1,7 @@
 #ifndef __CCLIB_GPUPARTICLE_INCLUDED__
 #define __CCLIB_GPUPARTICLE_INCLUDED__
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <tr1/memory>
-#include <GL/glew.h>
-
-#include <math/vec3.h>
-#include <gl/texture2d.h>
-#include <gl/shader.h>
-#include <gl/gpuparticles.h>
-#include <Exception.h>
+#include <cclib.h>
 
 namespace cclib {
 
