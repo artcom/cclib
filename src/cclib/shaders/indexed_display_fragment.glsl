b7aa28664d7801f518fac776fb154999a9fdfe45
Valentin Schunack <valentin.schunack@artcom.de>
2013-06-03 16:55:06 +0200

    added multiple render target support

--- src/cclib/shaders/indexed_display_fragment.glsl
+++ src/cclib/shaders/indexed_display_fragment.glsl
@@ -1,9 +1,9 @@
 #version 120 
 #extension GL_ARB_texture_rectangle : enable
 
-
-
 void main(){
 	
-	gl_FragColor = gl_Color;
+	gl_FragData[0] = gl_Color;
+	gl_FragData[1] = gl_Color;//vec4(1.0,0.0,0.0,1.0);
+
 }
