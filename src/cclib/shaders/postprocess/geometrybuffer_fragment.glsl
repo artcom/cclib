8a8d0dfe059b326097ab8cea12c860b798fc0245
Frank <frank.eickhoff@artcom.de>
2013-06-27 17:06:24 +0200

    geometry shader with spot lighting

--- src/cclib/shaders/postprocess/geometrybuffer_fragment.glsl
+++ src/cclib/shaders/postprocess/geometrybuffer_fragment.glsl
@@ -4,14 +4,106 @@ varying vec4 albedo;
 varying vec4 diffuse;
 varying vec4 specular;
 varying vec4 pos;
+varying vec4 posinv;
 varying vec3 normal;
 varying float depth;
 
 uniform sampler2D colorTexture; // GL_TEXTURE_2D
 //uniform sampler2DRect colorTexture; // GL_TEXTURE_RECTANGLE
 
+varying vec4 ambientGlobal, ambient;
+varying vec3 lightDir,halfVector;
+varying float dist;
+
+// vec4 generic_lighting(vec4 vertex_e, vec4 color, vec3 normal_e)
+// {
+// 	// Return lighted color of vertex_e.
+// 	//
+// 	vec4 light_pos = gl_LightSource[0].position;
+// 	vec3 v_vtx_light = light_pos.xyz - vertex_e.xyz;
+// 	float d_n_ve = -dot(normal_e,vertex_e.xyz);
+// 	float d_n_vl = dot(normal_e, normalize(v_vtx_light).xyz);
+// 	bool same_sign = ( d_n_ve > 0 ) == ( d_n_vl > 0 );
+// 	float phase_light = same_sign ? abs(d_n_vl) : 0;
+// 
+// 	vec3 ambient_light = gl_LightSource[0].ambient.rgb;
+// 	vec3 diffuse_light = gl_LightSource[0].diffuse.rgb;
+// 	float dist = length(v_vtx_light);
+// 	float distsq = dist * dist;
+// 	float atten_inv =
+// 	gl_LightSource[0].constantAttenuation +
+// 	gl_LightSource[0].linearAttenuation * dist +
+// 	gl_LightSource[0].quadraticAttenuation * distsq;
+// 	vec4 lighted_color;
+// 	lighted_color.rgb =
+// 	color.rgb * gl_LightModel.ambient.rgb
+// 	+ color.rgb * ( ambient_light + phase_light * diffuse_light ) / atten_inv;
+// 	lighted_color.a = color.a;
+// 	return lighted_color;
+// }
+// 
+// #define MAX_LIGHTS 2 
+// 
+// vec4 multi_lighting()
+// {
+// 	vec3 N = normal;
+// 	vec3 v = pos.xyz;
+// 	vec4 finalColour;
+// 
+// 	for (int i=0; i<MAX_LIGHTS; i++)
+// 	{
+// 		vec3 L = normalize(gl_LightSource[i].position.xyz - v);   
+// 		vec3 E = normalize(-v);
+// 		vec3 R = normalize(-reflect(L,N));  
+// 		vec4 Iamb = gl_FrontLightProduct[i].ambient;    
+// 		vec4 Idiff = gl_FrontLightProduct[i].diffuse * max(dot(N,L), 0.0);
+// 		Idiff = clamp(Idiff, 0.0, 1.0);     
+// 		vec4 Ispec = gl_FrontLightProduct[i].specular 
+// 					* pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess);
+// 		Ispec = clamp(Ispec, 0.0, 1.0);
+// 		finalColour += Iamb + Idiff + Ispec;
+// 	}
+// // 	gl_FragColor = gl_FrontLightModelProduct.sceneColor + finalColour;
+// 	return finalColour; 
+// }
+
+vec4 spot_lighting()
+{
+	vec3 n,halfV;
+	float NdotL,NdotHV;
+	vec4 color = ambientGlobal;
+	float att,spotEffect;
+
+	/* a fragment shader can't write a verying variable, hence we need
+	a new variable to store the normalized interpolated normal */
+	n = normalize(normal);
+
+	/* compute the dot product between normal and ldir */
+	NdotL = max(dot(n,normalize(lightDir)),0.0);
+
+	if (NdotL > 0.0) {
+
+		spotEffect = dot(normalize(gl_LightSource[0].spotDirection), normalize(-lightDir));
+		if (spotEffect > gl_LightSource[0].spotCosCutoff) {
+			spotEffect = pow(spotEffect, gl_LightSource[0].spotExponent);
+			att = spotEffect / (gl_LightSource[0].constantAttenuation +
+			gl_LightSource[0].linearAttenuation * dist +
+			gl_LightSource[0].quadraticAttenuation * dist * dist);
+
+			color += att * (diffuse * NdotL + ambient);
+
+
+			halfV = normalize(halfVector);
+			NdotHV = max(dot(n,halfV),0.0);
+			color += att * gl_FrontMaterial.specular * gl_LightSource[0].specular * pow(NdotHV,gl_FrontMaterial.shininess);
+		}
+	}
+
+	return color;
+}
+
 void main(){
-	gl_FragData[0] = pos;
+	gl_FragData[0] = posinv;
 
 	gl_FragData[1] = vec4(
 		normal.x * 0.5f + 0.5f, 
@@ -29,7 +121,17 @@ void main(){
 	);
 	*/
 
-	gl_FragData[2] = gl_Color * texture2D(colorTexture, vec2(gl_TexCoord[0].x, gl_TexCoord[0].y));
+// 	gl_FragData[2] = gl_Color;
+// 	gl_FragData[2] = gl_Color * texture2D(colorTexture, vec2(gl_TexCoord[0].x, gl_TexCoord[0].y));
+	
+	vec4 myColor = texture2D(colorTexture, vec2(gl_TexCoord[0].x, gl_TexCoord[0].y));
+
+// 	gl_FragData[2] = gl_Color * generic_lighting( pos, myColor, normalize(normal));
+// 	gl_FragData[2] = myColor * multi_lighting();
+	gl_FragData[2] = myColor * spot_lighting();
+	
 	//gl_FragData[2] = gl_Color * texture2D(colorTexture, vec2(gl_TexCoord[0].x, 1.0 - gl_TexCoord[0].y));
 	//gl_FragData[2] = texture2DRect(colorTexture, gl_TexCoord[0].xy);
+	
+	
 }
--- src/cclib/shaders/postprocess/geometrybuffer_vertex.glsl
+++ src/cclib/shaders/postprocess/geometrybuffer_vertex.glsl
@@ -1,6 +1,7 @@
 #version 120
 
 varying vec4 pos;
+varying vec4 posinv;
 varying vec3 normal;
 varying float depth;
 
@@ -12,12 +13,50 @@ uniform float near;
 uniform float far;
 uniform mat4 inverseView;
 
+varying vec4 ambientGlobal, ambient;
+varying vec3 lightDir,halfVector;
+varying float dist;
+
+// vec4 spot_lighting()
+// {
+// 	//vec3  lightPositionOC = gl_LightSource[0].position;   // in object coordinates
+// 	//vec3  spotDirectionOC = gl_LightSource[0].position;   // in object coordinates
+// 	float spotCutoff = gl_LightSource[0].spotCutoff;        // in degrees
+// 
+// 	vec3 lightPosition;
+// 	vec3 spotDirection;
+// 	vec3 lightDirection;
+// 	float angle;
+// 
+// 	// Transforms light position and direction into eye coordinates
+// 	//lightPosition  = (lightPositionOC * gl_ModelViewMatrix).xyz;
+// 	//spotDirection  = normalize(spotDirectionOC * gl_NormalMatrix);
+// 	
+// 	lightPosition = gl_LightSource[0].position.xyz;
+// 	spotDirection = gl_LightSource[0].spotDirection;
+// 	
+// 	// Calculates the light vector (vector from light position to vertex)
+// 	vec4 vertex = gl_ModelViewMatrix * gl_Vertex;
+// 	lightDirection = normalize(vertex.xyz - lightPosition.xyz);
+// 
+// 	// Calculates the angle between the spot light direction vector and the light vector
+// 	angle = dot( normalize(spotDirection),
+// 			-normalize(lightDirection));
+// 	angle = max(angle,0);   
+// 
+// 	// Test whether vertex is located in the cone
+// 	if(angle > radians(spotCutoff))
+// 		return vec4(1,1,0,1); // lit (yellow)
+// 	else
+// 		return vec4(0.5,0.5,0.5,1); // unlit
+// }
+
 void main(){
 	pos	= gl_ModelViewMatrix * gl_Vertex;
-	pos = inverseView * pos;
-	//pos	= gl_ModelViewMatrix * gl_Vertex;
+	posinv = inverseView * pos;
+	
 	normal = normalize(gl_NormalMatrix * gl_Normal);
-	depth = 1-(-pos.z - near) / (far - near);
+	depth = 1-(-posinv.z - near) / (far - near);
 
 	albedo = gl_FrontMaterial.diffuse;
 	diffuse	= gl_FrontMaterial.diffuse;
@@ -25,8 +64,26 @@ void main(){
 	specular.a = gl_FrontMaterial.shininess;
 	//albedo = gl_FrontMaterial.emission + (gl_LightModel.ambient * gl_FrontMaterial.ambient);
 	
+	
+	// https://github.com/silam/OutForDrive/blob/master/spotlight-vshader-transform.glsl
+	
+	vec3 aux = vec3(gl_LightSource[0].position-pos);
+	lightDir = normalize(aux);
+	/* compute the distance to the light source to a varying variable*/
+	dist = length(aux);
+	/* Normalize the halfVector to pass it to the fragment shader */
+	halfVector = normalize(gl_LightSource[0].halfVector.xyz);
+
+	/* Compute the diffuse, ambient and globalAmbient terms */
+	diffuse = gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse;
+	ambient = gl_FrontMaterial.ambient * gl_LightSource[0].ambient;
+	ambientGlobal = gl_LightModel.ambient * gl_FrontMaterial.ambient;
+
+	
+	
 	gl_TexCoord[0] = gl_MultiTexCoord0;
 
 	gl_Position  = gl_ModelViewProjectionMatrix * gl_Vertex;
 	gl_FrontColor = gl_Color;
+// 	gl_FrontColor = spot_lighting();
 }
