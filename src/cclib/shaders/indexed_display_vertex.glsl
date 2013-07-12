f471f529e369debf8614609564b2be340704f939
Frank <frank.eickhoff@artcom.de>
2013-07-12 12:03:11 +0200

    fixed floating point

--- src/cclib/shaders/indexed_display_vertex.glsl
+++ src/cclib/shaders/indexed_display_vertex.glsl
@@ -15,27 +15,27 @@ void main ()
 {
 	vec4 myPosition = texture2DRect(positions, gl_Vertex.xy);
 	gl_Position = gl_ModelViewProjectionMatrix * myPosition;
-	gl_TexCoord[0] = vec4(gl_Vertex.xy,0,0);
+	gl_TexCoord[0] = vec4(gl_Vertex.xy,0.0,0.0);
 
 	vec4 myValues = texture2DRect(infos, gl_Vertex.xy);
 
-	float myAlpha = clamp(1 - myValues.x / myValues.y * (1 - myValues.z), 0.5, 1); // <-
+	float myAlpha = clamp(1.0 - myValues.x / myValues.y * (1 - myValues.z), 0.5, 1.0); // <-
 	gl_FrontColor = texture2DRect(colors, gl_Vertex.xy) * gl_Color;
 // 	gl_FrontColor.a = 1;
 	gl_FrontColor.a = myAlpha;
 
 	// Compute point size.
 	vec4 posViewSpace = gl_ModelViewMatrix * myPosition;
-	float myPointSize = max(tanHalfFOV / -posViewSpace.z * pointSize * myValues.y, 1);
+	float myPointSize = max(tanHalfFOV / -posViewSpace.z * pointSize * myValues.y, 1.0);
 //     float myPointSize = max(tanHalfFOV / -posViewSpace.z * myValues.y, 1) * pointSize;
 	gl_PointSize = myPointSize;
 
 
 
-    float blurDepth =  3000;
+    float blurDepth =  3000.0;
     float depth = (-posViewSpace.z)/(blurDepth);
     float blur = 1.0 - clamp(depth, 0.0, 1.0);
-	gl_TexCoord[1] = vec4(blur,blur,blur,1);
+	gl_TexCoord[1] = vec4(blur,blur,blur,1.0);
 
     /*float far =  3000;//10000.0;
     float depth = (-posViewSpace.z)/(far); // will map near..far to 0..1
